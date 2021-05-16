c0cb6424a86c1af4bb32cd7f6663eb166326c81f
Caroline <dev.caroline.ribeiro@gmail.com>
2021-05-15 21:57:24 -0300

    wip

--- src/application/jwt.ts
+++ src/application/jwt.ts
@@ -2,12 +2,12 @@ import expressJwt from "express-jwt";
 
 export default function jwt() {
     const secret = process.env.SECRET;
-    return expressJwt({ secret, algorithms: ['HS256'] }).unless({
-        path: [
-            '/user',
-            '/login',
-            '/health',
-            '/agv/helix',
-        ]
+    return expressJwt({ secret, algorithms: ['HS256'] }).unless((req: any) => {
+        return (
+            req.originalUrl === "/user" && req.method === "POST" ||
+            req.originalUrl === "/login" && req.method === "POST" ||
+            req.originalUrl === "/health" && req.method === "GET" ||
+            req.originalUrl === "//agv/helix" && req.method === "GET" 
+        )
     });
 }
